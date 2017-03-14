a78b10e416fad72f72b2c66714645fb8228762f2
Elissa Besta <elissa.besta@gmail.com>
2017-03-14 22:47:04 +0000

    with router

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+
+  get 'reviews/create'
+
+  get 'reviews/destroy'
+
+  root to: 'cocktails#index'
+  resources :cocktails do
+    resources :doses, only: [:new, :create]
+    resources :reviews, only: [:new, :create]
+  end
+
+  resources :doses, only: [:destroy]
+  resources :reviews, only: [:destroy]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-
-# config/routes.rb
-mount Attachinary::Engine => "/attachinary"
+  mount Attachinary::Engine => "/attachinary"
 end
